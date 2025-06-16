ee763ee3c84c73b907cfd7efc5ffbe53f86eef22
Nahom Kenna <nahomkenna@protonmail.com>
2025-06-16 10:49:07 -0400

    Refactor: Move Terraform and Lambda code to root directory

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_prefix" {
+  description = "Prefix for the S3 bucket name"
+  type        = string
+  default     = "rearc-pipeline-bucket"
+}
